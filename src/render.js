a49ec72a55cf33c5f261dadd14102e84db8a3f25
chnirt <trinhchinchin@gmail.com>
2020-10-26 16:09:55 +0700

    first commit

---
+++ src/render.js
@@ -0,0 +1,14 @@
+const loginButton = document.getElementById('loginButton')
+loginButton.onClick = handleLogin
+const registerButton = document.getElementById('registerButton')
+registerButton.onClick = handleRegister
+
+function handleLogin() {
+	console.log('Login')
+}
+function handleRegister() {
+	console.log('Register')
+}
+
+function navigateLogin() {}
+function navigateRegister() {}
